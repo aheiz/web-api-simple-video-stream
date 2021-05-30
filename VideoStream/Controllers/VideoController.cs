e6d6e48922763155322ddceddaffd1f79cf84018
Andreas Heizmann <ah@interap.de>
2021-05-30 10:58:04 +0200

    Add project files.

---
+++ VideoStream/Controllers/VideoController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.IO;
+
+namespace VideoStream.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class VideoController : ControllerBase
+    {
+        private readonly ILogger<VideoController> _logger;
+
+        public VideoController(
+            ILogger<VideoController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("stream/{*path}")]
+        public FileResult GetStreamFromPath(string path)
+        {
+            string filepath = Path.Combine("Content", path);
+
+            return new VirtualFileResult(filepath, "video/mp4");
+        }
+    }
+}
---
+++ VideoStream/Startup.cs
@@ -0,0 +1,54 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace VideoStream
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "VideoStream", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "VideoStream v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseFileServer();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
